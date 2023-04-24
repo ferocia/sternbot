5a722a97f3d06f42a02331585b89c1dedcb1a2f8
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2023-04-24 11:32:15 +1000

    notify slack when a user couldn't be added

--- app/jobs/add_player_job.rb
+++ app/jobs/add_player_job.rb
@@ -4,5 +4,13 @@ class AddPlayerJob < ApplicationJob
   def perform(username)
     player = Syncer.add_player!(username)
     SlackNotifier.send_message("Added #{username} as #{player.tag}")
+  rescue => e
+    # TODO: should probably have a "why" here???
+    SlackNotifier.send_message(<<~EOF)
+      Failed to add #{username}.
+
+      This is either because the username was wrong, or that user doesn't have their stats visible to the public.
+      If this is you, you can go <https://insider.sternpinball.com/account/profile/stats|here> and make sure the checkbox is selected.
+    EOF
   end
 end
